e2180228a7856bdc63c1765ecf53caf45f57658f
Honey1503 <simranshandge1998@gmail.com>
2023-02-12 23:09:33 +0530

    Created correction for prime and palindrome

--- prime.js
+++ prime.js
@@ -1,9 +1,11 @@
-function checkPrime(num){
-for(let i=i;i<=num;i++){
-if(num%i){
-return true;
+let number=7;
+let factor=0;
+for(let i=1;i<=number;i++)
+{
+ if(number%i==0)
+{
+factor++;
 }
 }
-return false
-}
-checkPrime(13)
+
+(factor==2)?console.log("It is a prime number"):console.log("It is not a prime number");
